fbf56df61e7b34f489f409a722fd0a7603061d95
Shruti Patil <shrutipatil6599@gmail.com>
2026-06-19 20:42:30 +0530

    Intial Commit

--- src/main/java/com/payroll/PayrollManagementSystemApplication.java
+++ src/main/java/com/payroll/PayrollManagementSystemApplication.java
@@ -2,12 +2,15 @@ package com.payroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+
 public class PayrollManagementSystemApplication {
 
 	public static void main(String[] args) {
